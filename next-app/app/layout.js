17b71774a3574ad95b4145b6fd9839b35bbbc86d
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-05-09 00:50:55 -0300

    .

--- next-app/app/layout.js
+++ next-app/app/layout.js
@@ -12,6 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script src="/socket.io/socket.io.js"></script>
+      </head>
       <body className={inter.className}>
         {children}
         <script src="@/scripts/socket-app.js"/>
