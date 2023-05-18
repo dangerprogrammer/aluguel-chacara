166c1868b8208ca2f99e37780d958f47babf2757
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-05-18 01:49:08 -0300

    .

--- client/components/context/ContextApp.jsx
+++ client/components/context/ContextApp.jsx
@@ -7,9 +7,9 @@ function ContextProvider({children}) {
         [login, setLogin] = useState(!1),
         [users, setUsers] = useState([]),
         routes = [
-            {text: "Meus aluguéis", path: '/myRent', iconName: 'home-outline', DescPath: ({given_name}) => `Página onde fica a lista dos aluguéis realizados por ${given_name}.`},
+            {text: "Meus aluguéis", path: '/my-rent', iconName: 'home-outline', DescPath: ({given_name}) => `Página onde fica a lista dos aluguéis realizados por ${given_name}.`},
             {text: "Calendário", path: '/calendar', iconName: 'calendar-number-outline', DescPath: () => 'Visualização em tempo real do calendário da chácara e criação de novos aluguéis.'},
-            {text: "Conversas", path: '/chats', iconName: 'chatbubbles-outline', DescPath: ({given_name}) => `Aqui é onde ficam as conversas de ${given_name} com outros usuários.`}
+            {text: "Minhas Conversas", path: '/chats', iconName: 'chatbubbles-outline', DescPath: ({given_name}) => `Aqui é onde ficam as conversas de ${given_name} com outros usuários.`}
         ],
         [sideuser, setSideuser] = useState(!1),
         values = {socket, setSocket, login, setLogin, users, setUsers, routes, sideuser, setSideuser};
